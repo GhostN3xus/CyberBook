485c1b63fa59ac5e1a1b45d05186b905a68133fc
Santiago <73343925+GhostN3xus@users.noreply.github.com>
2025-10-29 18:23:22 -0300

    Delete api directory

--- api/csp-report/index.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = async function (context, req) {
-  // Recebe relatórios CSP (report-uri /report-to). Apenas loga (sem persistência).
-  const body = req.body || {};
-  context.log('CSP-Report', JSON.stringify(body));
-  context.res = { status: 204 };
-};
